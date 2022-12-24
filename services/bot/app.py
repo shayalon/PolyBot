f85e8cabb85179cb814bf6431d9bbc00e468bceb
ShayKllifAlon <kllif@hotmail.com>
2022-12-24 17:24:26 +0200

    remove comment

--- services/bot/app.py
+++ services/bot/app.py
@@ -77,7 +77,6 @@ if __name__ == '__main__':
 
     sqs = boto3.resource('sqs', region_name=config.get('aws_region'))
     workers_queue = sqs.get_queue_by_name(QueueName=config.get('bot_to_worker_queue_name'))
-    #testing pr
     my_bot = YoutubeObjectDetectBot(_token)
     my_bot.send_text(my_bot.updater, "greetings :-)")
     my_bot.start()
